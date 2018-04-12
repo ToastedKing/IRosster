7bc6c8f76bc7765f3d071b12bfe6367eaccd9ed2
Bryan Ayala <ayala.bryan9@gmail.com>
2018-04-12 13:08:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ayala/irosster/RosterActivity.java
@@ -0,0 +1,13 @@
+package com.example.ayala.irosster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RosterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roster);
+    }
+}
